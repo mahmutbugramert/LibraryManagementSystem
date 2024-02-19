579bea5cfc88b5dc89d74766346fdb64a023debc
Mahmut Buğra Mert <124915257+mahmutbugramert@users.noreply.github.com>
2024-02-20 00:39:34 +0300

    Update LMS.py

--- LMS.py
+++ LMS.py
@@ -1,17 +1,25 @@
 class Library:
-    file = "books.txt"
-    list = []
+    file = None
     def __init__(self): #constructor method
-        self.file = open("books.txt", "r+") #"a+" did not work for me. So I used "r+".
-    def __del__(self): #NOT-COMPLETE
+        self.file = open("books.txt", "a+") 
+
+    def __del__(self): #destructor method
         self.file.close()
 
     def listBooks(self):
-        lines = self.file.read().splitlines()
-        for elt in lines:
-            print("book:",elt.split(",")[0],", author:",elt.split(",")[1])   
+        self.file.seek(0)
+        myFile = self.file.read()
+        lines = myFile.splitlines()
+        if(len(lines) == 0):
+            print("------------------\nNo book in databese!\n------------------")
+        else:
+            print("------------------")
+            for elt in lines:
+                print("book:",elt.split(",")[0],", author:",elt.split(",")[1])
+            print("------------------")
 
     def addBook(self):
+        self.file.seek(0)
         title = input("Please enter book title:")
         author = input("Please enter book author:")
         year = input("Please enter first release year:")
@@ -19,25 +27,41 @@ class Library:
 
         newLine = title + "," + author + "," + year + "," + numOfPage + "\n"
 
+        self.file.seek(0, 2)
         self.file.write(newLine)
-
+        
     def removeBook(self):
+        self.file.seek(0)
         title = input("Please enter the book title you want to delete:")
-        lines = self.file.read().splitlines()
-        print(lines)
+        myFile = self.file.read()
+        lines = myFile.splitlines()
+        flag = True
         for i in range(len(lines)):
             if(lines[i].split(",")[0] == title):
-                 lines.remove(i)
+                 flag = False
+                 lines.pop(i)
+                 break
+        if(flag):
+            print("----------------------\nNo book named", title, "in the database!\n----------------------")
+        self.file.seek(0)
         self.file.truncate(0)
         for elt in lines:
             self.file.write(elt+"\n")
 
 
-myLib = Library()
-myLib.listBooks()
-# myLib.addBook()
-# myLib.addBook()
-# myLib.addBook()
-print(myLib.file.read())
-myLib.removeBook()
+lib = Library()
+
+print("Welcome to the Library Management System!")
+while True:
+    operation = input("*** MENU***\n1) List Books\n2) Add Book\n3) Remove Book\n4) Exit\n")
+    if(operation == "1"):
+        lib.listBooks()
+    elif(operation == "2"):
+        lib.addBook()
+    elif(operation == "3"):
+        lib.removeBook()
+    elif(operation == "4"):
+        break
+    else:
+        print("------------------------------\nUnrecognised input. Try again!\n------------------------------")
 
